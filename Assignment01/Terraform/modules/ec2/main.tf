9449b1f4203f7c337ab8b251def884fb42b9b03f
VuDucTruong <21520507@gm.uit.edu.vn>
2024-11-03 23:27:35 +0700

    Update main.tf

--- Assignment01/Terraform/modules/ec2/main.tf
+++ Assignment01/Terraform/modules/ec2/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "public_instance" {
   instance_type               = var.instance_type
   subnet_id                   = var.public_subnet_id
   associate_public_ip_address = false
-  vpc_security_group_ids      = [var.public_ec2_sg_id] # Sử dụng vpc_security_group_ids thay vì security_groups
+  security_groups             = [var.public_ec2_sg_id] # Sử dụng vpc_security_group_ids thay vì security_groups
   ebs_optimized               = true
   monitoring                  = true
   iam_instance_profile        = "test"
@@ -22,13 +22,13 @@ resource "aws_instance" "public_instance" {
 
 # Tạo Private EC2 Instance
 resource "aws_instance" "private_instance" {
-  ami                    = var.ami_id # Thay thế bằng AMI ID thực tế
-  instance_type          = var.instance_type
-  subnet_id              = var.private_subnet_id
-  vpc_security_group_ids = [var.private_ec2_sg_id] # Sử dụng vpc_security_group_ids thay vì security_groups
-  ebs_optimized          = true
-  monitoring             = true
-  iam_instance_profile   = "test"
+  ami                  = var.ami_id # Thay thế bằng AMI ID thực tế
+  instance_type        = var.instance_type
+  subnet_id            = var.private_subnet_id
+  security_groups      = [var.private_ec2_sg_id] # Sử dụng vpc_security_group_ids thay vì security_groups
+  ebs_optimized        = true
+  monitoring           = true
+  iam_instance_profile = "test"
   metadata_options {
     http_endpoint = "enabled"
     http_tokens   = "required" # Enables only IMDSv2
